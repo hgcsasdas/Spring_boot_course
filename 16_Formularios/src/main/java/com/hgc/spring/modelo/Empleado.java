7e9a5d271084f9f4e2598e671db989f36323df25
Hgc88 <carl.san.nu@gmail.com>
2023-11-08 23:44:41 +0100

    hecho hasta seguridad

---
+++ 16_Formularios/src/main/java/com/hgc/spring/modelo/Empleado.java
@@ -0,0 +1,110 @@
+package com.hgc.spring.modelo;
+
+import java.util.Objects;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+
+public class Empleado {
+
+	@Min(value=0, message="{empleado.id.mayorquecero}")
+	private long id;
+	@NotEmpty
+	private String nombre;
+	@Email
+	private String email;
+	
+	private String telefono;
+
+	private String imagen;
+	
+	public Empleado() {
+		super();
+	}
+
+	public Empleado(long id, String nombre, String email, String telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
+		this.telefono = telefono;
+	}
+
+	public Empleado(@Min(value = 0, message = "{empleado.id.mayorquecero}") long id, @NotEmpty String nombre,
+			@Email String email, String telefono, String imagen) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.email = email;
+		this.telefono = telefono;
+		this.imagen = imagen;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, imagen, nombre, telefono);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		return Objects.equals(email, other.email) && id == other.id && Objects.equals(imagen, other.imagen)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(telefono, other.telefono);
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [id=" + id + ", nombre=" + nombre + ", email=" + email + ", telefono=" + telefono + ", imagen="
+				+ imagen + "]";
+	}
+
+
+
+}
